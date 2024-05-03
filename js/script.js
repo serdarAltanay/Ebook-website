1f0ad22795d43fcad55fe9ea21586dbcd2fe1d5d
Serdar Altanay <serdaraltanay@gmail.com>
2024-05-03 11:27:56 +0300

    navbar modified

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 // Add custom JavaScript here
+function userScroll() {
+    const navbar = document.querySelector(".navbar")
+
+    window.addEventListener("scroll" , () => {
+        if (window.scrollY > 50){
+            navbar.classList.add("bg-dark")
+            navbar.classList.add("opacity-75")
+            navbar.classList.add("shadow")
+
+
+        } else{
+            navbar.classList.remove("bg-dark")
+            navbar.classList.remove("opacity-75")
+            navbar.classList.remove("shadow")
+
+
+        }
+    })
+}
+document.addEventListener("DOMContentLoaded",userScroll)
